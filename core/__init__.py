47a41df930d62fa57862ff83e196302bdac49fd0
CatCat <>
2026-06-02 16:12:29 +0800

    新增CloudflareWorker+R2图床

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,7 @@ from .base import BaseProvider
 from .downloader import Downloader
 from .gemini import GeminiProvider
 from .http_manager import HttpManager
+from .image_hosting import R2ImageHoster
 from .agnes_images import AgnesImagesProvider
 from .openai_chat import OpenAIChatProvider
 from .openai_images import OpenAIImagesProvider
@@ -12,6 +13,7 @@ __all__ = [
     "Downloader",
     "BaseProvider",
     "GeminiProvider",
+    "R2ImageHoster",
     "AgnesImagesProvider",
     "OpenAIChatProvider",
     "OpenAIImagesProvider",
